19c9e7f6dbca4e166127997f2a08b1d0ff60a103
eomgn <matheeusgn21@gmail.com>
2025-05-28 23:10:19 -0300

    :ok_hand: chore: adjuste date actual in page schedule

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,5 +1,12 @@
+import dayjs from "dayjs";
 import { openHours } from "../form/hours.js";
 
+// data na pagina inicial de agenda
+const today = dayjs(new Date()).format("YYYY-MM-DD");
+const dateView = document.querySelector("#date");
+dateView.value = today;
+
+// data do formulário
 const dateSchedule = document.querySelector("#date-schedule");
 
 export function load() {
